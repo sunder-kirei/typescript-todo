8187e6a4b0836d7cbfeec3c79fe7e51206bfb137
Sunder Kumar <sunderkumar2506@gmail.com>
2022-12-29 19:20:00 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import TodoProvider from "./providers/TodoProvider";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoProvider>
+      <AddTodo />
+      <TodoList />
+    </TodoProvider>
   );
-}
+};
 
 export default App;
